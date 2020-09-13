4c2542d401dc81218e0879e002a321907a58302e
Adam Austin <ataustin@gmail.com>
2020-09-12 21:23:17 -0500

    add examples of functionality

--- benchmarking/sqrt-bench.R
+++ benchmarking/sqrt-bench.R
@@ -4,7 +4,8 @@ bm <-
   bench::mark(
     sqrt(input),
     input^0.5,
-    exp(0.5 * log(input))
+    exp(0.5 * log(input)),
+    take_sqrt(input)  # the function we wrote earlier
   )
 
 bm
@@ -14,3 +15,6 @@ tibble::glimpse(bm)
 plot(bm)  # requires tidyr,
           # ggplot2,
           # and ggbeeswarm
+
+plot(bm, type = "boxplot")
+plot(bm, type = "ridge")   # requires ggridges
--- profiling/profvis.R
+++ profiling/profvis.R
@@ -11,4 +11,16 @@ profvis::profvis(tour_house())
 
 source("profiling/take-sqrt.R")
 
-profvis::profvis(take_sqrt(1:5e7))
+profvis::profvis(take_sqrt(1:(3e7)))
+
+
+
+
+
+
+
+
+
+profvis::profvis(replicate(1e6, 1 + 2))
+profvis::profvis(replicate(1e6, base::`+`(1, 2)))
+
